dd3f843dcf53b1f0022e3d44d8ddab16721c39ae
Hector Mendoza <hector.mendozam@alumno.buap.mx>
2025-10-06 21:41:26 -0600

    Ahora si

--- build.sh
+++ build.sh
@@ -3,8 +3,15 @@
 # Salir inmediatamente si un comando falla
 set -o errexit
 
-# Instalar dependencias
+# 1. Instalar dependencias
 pip install -r requirements.txt
 
-# Recolectar archivos estáticos
+# 2. EJECUTAR MIGRACIONES
+# Nota: La base de datos debe estar configurada en settings.py para este punto.
+python manage.py makemigrations dashboard
+python manage.py migrate --no-input
+
+# 3. Recolectar archivos estáticos
 python manage.py collectstatic --no-input
+
+# ¡Ya no necesitas el shell de Render para esto!
